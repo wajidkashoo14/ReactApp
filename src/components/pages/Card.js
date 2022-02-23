2f08ad38cd3ffe6fd96035670a3c4672bc4fa01e
wajidkashoo14 <wajidkashoo14@gmail.com>
2022-02-23 16:03:11 +0530

    added some styles

--- src/components/pages/Card.js
+++ src/components/pages/Card.js
@@ -1,14 +1,14 @@
 import React from "react";
 import './Card.css'
 
-function Card({ img, title, info, btn }) {
+function Card({ img, title, info, }) {
   return (
     <>
       <div className="card">
-        <img src={img} alt={title} />
+        <img className="cardTech-img" src={img} alt={title} />
           <h4>{title}</h4>
           <p>{info}</p>
-          {btn && <button>hi</button>}
+          
         </div>
     </>
   );
